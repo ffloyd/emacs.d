401d29893ee5ef6c3ca6395ef40ca825f9a1f514
Roman Kolesnev <rvkolesnev@gmail.com>
2018-01-04 03:58:45 +0300

    Add magithub support

--- ffe/ffe-git.el
+++ ffe/ffe-git.el
@@ -12,12 +12,18 @@
 
 (use-package evil-magit)
 
+(use-package magithub
+  :config
+  (magithub-feature-autoinject t))
+
 (use-package gitignore-mode
   :config
   (add-hook 'gitignore-mode-hook
             (lambda ()
               (setq-local company-backends '(company-files)))))
+
 (use-package gitconfig-mode)
+
 (use-package gitattributes-mode)
 
 (provide 'ffe-git)
